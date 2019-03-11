886d903915dadf554f33ab38d7c2da61d51b5983
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-03-11 08:58:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,6 +10,12 @@ post '/team' do
 
 erb :team
 @team_name = params[:name]
+@coach = params[:coach]
+@point_guard = params[:name]
+@shooting_guard = params[:name]
+@small_forward = params[:name]
+@power_forward = params[:name]
+@center = params[:name]
   erb :team
 end
 
